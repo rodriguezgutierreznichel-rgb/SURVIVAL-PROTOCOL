d4f128074f2df29ee8793c0e32b83e9455c82b2c
rodriguezgutierreznichel-rgb <rodriguezgutierreznichel@gmail.com>
2026-01-29 21:10:21 +0000

    La implementación del player y primeras animaciones del mismo
    Se ha añadido al player y se ha implementado los movimientos del idle y el run del player, hay fallas a la hora de correr, ya que el personaje corre de lado y no de frente.

---
+++ Assets/SCRIPS/PLAYER/PlayerMove.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshAgent agent;
+
+    [SerializeField]
+    Animator animator;
+
+    public float movimientoEjeX;
+    public float movimientoEjeY;
+    public float movimientoEjeZ;
+
+    public float velocidadDelMovimiento = 0;
+
+    
+
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+
+    }
+
+    void Update()
+    {
+        PersonajeMove();
+    }
+
+    public void PersonajeMove()
+    {
+
+        movimientoEjeZ = Input.GetAxis("Horizontal") * Time.deltaTime * velocidadDelMovimiento;
+        movimientoEjeX = Input.GetAxis("Vertical") * Time.deltaTime * velocidadDelMovimiento;
+
+
+        transform.Translate(movimientoEjeZ, movimientoEjeY, movimientoEjeX);
+
+
+        if (movimientoEjeZ != 0 || movimientoEjeX != 0)
+        {
+            animator.SetBool("RUN", true);
+        }
+        else
+        {
+            animator.SetBool("RUN", false);
+        }
+    }
+}
